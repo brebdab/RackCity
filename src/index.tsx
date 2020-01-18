e761b9a92add611a009f93f96ed7dbe827d8ab09
clairecfu <47611824+clairecfu@users.noreply.github.com>
2020-01-18 14:38:35 -0500

    Initial project setup (#1)
    * django/react integration
    
    * adding heroku domains
    
    * adding hosts
    
    *  trying to fix get call
    
    * commenting out local host

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Navigation from './components/navigation/navigation';
+import * as serviceWorker from './serviceWorker';
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+const App = () => (
+    <div>
+        <Navigation />
+
+
+    </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
