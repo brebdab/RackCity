da349b7a4f56c60fa3b761a58a8d003a8a60269d
Julia Saveliff <jms247@duke.edu>
2020-01-21 23:36:09 -0500

    simple get/post for all models

--- rackcity/api/views.py
+++ rackcity/api/views.py
@@ -1,8 +1,40 @@
 from rest_framework import viewsets
-
-from rackcity.models import Article
-from .serializers import ArticleSerializer
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rackcity.models import (
+    Article, 
+    ITInstance,
+    ITModel, 
+    Rack, 
+    User
+)
+from rackcity.api.serializers import (
+    ArticleSerializer, 
+    ITInstanceSerializer, 
+    ITModelSerializer, 
+    RackSerializer, 
+    UserSerializer
+)
 
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+
+@csrf_exempt
+def model_list(request):
+    """
+    List all models, or create a new model.
+    """
+    if request.method == 'GET':
+        models = ITModel.objects.all()
+        serializer = ITModelSerializer(models, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ITModelSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
