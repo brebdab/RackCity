e69eae1b1d0e3bd3f65ac7d097bdb758ea6254ec
Julia Saveliff <jms247@duke.edu>
2020-01-21 23:52:41 -0500

    add models urls

--- rackcity/api/urls.py
+++ rackcity/api/urls.py
@@ -1,6 +1,12 @@
-from rackcity.api.views import ArticleViewSet
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from rackcity.api import views
+# from rackcity.api.views import ArticleViewSet
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'', ArticleViewSet, basename='rackcity')
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r'', ArticleViewSet, basename='rackcity')
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('models/', views.model_list),
+]
