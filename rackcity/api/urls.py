456bb85295bb9797f843e2fe3796676a4f1e81f2
Julia Saveliff <jms247@duke.edu>
2020-01-26 17:49:37 -0500

    implement /instances/id

--- rackcity/api/urls.py
+++ rackcity/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('models/test-admin', views.model_admin),
     path('instances', views.instance_list),
     path('instances/get-many', views.instance_page),
-    path('instances/<int:pk>', views.instance_detail),
+    path('instances/<int:id>', views.instance_detail),
     path('instances/add', views.instance_add),
     path('racks', views.rack_list),
     path('racks/<int:pk>', views.rack_detail),
