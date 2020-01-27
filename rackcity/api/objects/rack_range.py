e4d78dce42baff25e93d2be499ac7ea4556c63d3
Julia Saveliff <jms247@duke.edu>
2020-01-27 02:09:30 -0500

    implement racks get

--- rackcity/api/objects/rack_range.py
+++ rackcity/api/objects/rack_range.py
@@ -6,3 +6,39 @@ class RackRangeSerializer(serializers.Serializer):
     letter_end = serializers.CharField(required=False, max_length=1)
     num_start = serializers.IntegerField(required=True)
     num_end = serializers.IntegerField(required=False)
+
+    def validate(self, data):
+        if ('num_end' in data) and (
+            data['num_end'] < data['num_start']
+        ):
+            message = "invalid rack number range from " + \
+                str(data['num_start']) + \
+                " to " + str(data['num_end'])
+            raise serializers.ValidationError(message)
+
+        if ('letter_end' in data) and (
+            data['letter_end'] < data['letter_start']
+        ):
+            message = "invalid rack row range from " + \
+                data['letter_start'] + " to " + data['letter_end']
+            raise serializers.ValidationError(message)
+
+        return data
+
+    def get_row_range(self, data):
+        """
+        Returns tuple specifying rack row range
+        """
+        if 'letter_end' in data:
+            return (data['letter_start'], data['letter_end'])
+        else:
+            return (data['letter_start'], data['letter_start'])
+
+    def get_number_range(self, data):
+        """
+        Returns tuple specifying rack number range
+        """
+        if 'num_end' in data:
+            return (data['num_start'], data['num_end'])
+        else:
+            return (data['num_start'], data['num_start'])
--- rackcity/views/rack_views.py
+++ rackcity/views/rack_views.py
@@ -1,7 +1,7 @@
 # from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
-from rackcity.models import Rack
-from rackcity.api.serializers import RackSerializer
+from rackcity.models import Rack, ITInstance
+from rackcity.api.serializers import RackSerializer, ITInstanceSerializer
 from rackcity.api.objects import RackRangeSerializer
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -38,11 +38,43 @@ def rack_get(request):
     """
     List all racks within specified range.
     """
-    rack_range_deserialized = RackRangeSerializer(data=request.data)
-    if not rack_range_deserialized.is_valid():
-        return JsonResponse({"failure_message": "Invalid body"}, status=HTTPStatus.BAD_REQUEST)
+    range_serializer = RackRangeSerializer(data=request.data)
+    if range_serializer.is_valid():
+        try:
+            range_serializer.validate(request.data)
+        except Exception as error:
+            return JsonResponse(
+                {"failure_message": str(error)},
+                status=HTTPStatus.BAD_REQUEST
+            )
+    else:
+        return JsonResponse(
+            {"failure_message": "invalid keys or values"},
+            status=HTTPStatus.BAD_REQUEST,
+        )
 
-    return JsonResponse({})
+    racks = Rack.objects.filter(
+        rack_num__range=range_serializer.get_number_range(request.data),
+        row_letter__range=range_serializer.get_row_range(request.data),
+    )
+    racks_with_instances = []
+    for rack in racks:
+        rack_serializer = RackSerializer(rack)
+        instances = ITInstance.objects.filter(rack=rack.id)
+        instances_serializer = ITInstanceSerializer(
+            instances,
+            many=True
+        )
+        rack_detail = {
+            "rack": rack_serializer.data,
+            "instances": instances_serializer.data,
+        }
+        racks_with_instances.append(rack_detail)
+
+    return JsonResponse(
+        {"racks": racks_with_instances},
+        status=HTTPStatus.OK
+    )
 
 
 @api_view(['POST'])
@@ -55,7 +87,7 @@ def rack_create(request):
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAdminUser])
 def rack_delete(request):
     """
     Delete racks within specified range.
