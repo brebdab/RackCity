82ec391b6b307d888c40c86af724c0bc4a3a0dd0
Julia Saveliff <jms247@duke.edu>
2020-01-27 19:30:01 -0500

    imple ment instance delete

--- rackcity/api/urls.py
+++ rackcity/api/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('instances/get-many', views.instance_page),
     path('instances/<int:id>', views.instance_detail),
     path('instances/add', views.instance_add),
+    path('instances/delete', views.instance_delete),
     path('instances/pages', views.instance_page_count),
     path('racks', views.rack_list),
     path('racks/<int:pk>', views.rack_detail),
