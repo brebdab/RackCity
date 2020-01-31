2cc225964bf269cb7e97464be6fddb2eaacb00f7
Brendan Cheng <bmc41@duke.edu>
2020-01-31 18:19:55 -0500

    Created tabular report

--- src/components/report/report.tsx
+++ src/components/report/report.tsx
@@ -1,4 +1,4 @@
-import { AnchorButton } from "@blueprintjs/core";
+import { Classes, Spinner, Card, Elevation } from "@blueprintjs/core";
 import * as React from "react";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import axios from "axios";
@@ -58,8 +58,69 @@ export class Report extends React.PureComponent<ReportProps & RouteComponentProp
       })
     }
     console.log(this.state)
+    const modelFields = {
+      vendor: "Vendor",
+      model_number: "Model Number",
+      alloc_pct: "Allocation %"
+    }
+    const ownerFields = {
+      owner: "Owner",
+      alloc_pct: "Allocation %"
+    }
+    const vendorFields = {
+      vendor: "Vendor",
+      alloc_pct: "Allocation %"
+    }
+    if (! this.state.state_loaded) {
+      return <Spinner size={Spinner.SIZE_LARGE}/>
+    } else {
+      return (
+        <div className={Classes.DARK}>
+          <h1>Free Rack Space Pct: {this.state.freeRack * 100}%</h1>
+          <h1>Model Allocation:</h1>
+          <Tabular data={this.state.model_allocation} fields={modelFields}/>
+          <h1>Owner Allocation:</h1>
+          <Tabular data={this.state.owner_allocation} fields={ownerFields}/>
+          <h1>Vendor Allocation:</h1>
+          <Tabular data={this.state.vendor_allocation} fields={vendorFields}/>
+        </div>
+      )
+    }
+  }
+}
+
+interface TabProps {
+  data: Array<Allocation>,
+  fields: any
+}
+
+class Tabular extends React.PureComponent<TabProps> {
+
+  render() {
     return (
-      <p>Report</p>
+      <Card elevation={Elevation.TWO}>
+        <table className={"bp3-html-table"}>
+          <thead>
+            <tr>
+              {Object.keys(this.props.data[0]).map((item: string) => {
+                return <th>{this.props.fields[item]}</th>
+              })}
+              <th>Allocation %</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data.map((entry: any) => {
+              return (
+                <tr>
+                  {Object.keys(entry).map((item: string) => {
+                    return <td>{entry[item]}</td>
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </Card>
     )
   }
 }
@@ -73,7 +134,6 @@ async function getReport(token: string) {
   return await axios
     .get(API_ROOT + "api/report", headers)
     .then(res => {
-      // console.log(res.data)
       return res.data
     })
 }
