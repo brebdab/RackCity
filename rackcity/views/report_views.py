d60373cbd14e3951c48e609a81fac03afc4b3c3e
Julia Saveliff <jms247@duke.edu>
2020-01-31 16:38:30 -0500

    update response format

--- rackcity/views/report_views.py
+++ rackcity/views/report_views.py
@@ -19,7 +19,7 @@ def report_rack_usage(request):
     instances = ITInstance.objects.all()
     for instance in instances:
         vendor = instance.model.vendor
-        model = vendor + instance.model.model_number
+        model = instance.model
         height = instance.model.height
         owner = instance.owner
         num_full_rack_slots += height
@@ -41,17 +41,30 @@ def report_rack_usage(request):
     rack_usage_data = {}
     rack_usage_data['free_rackspace_percent'] = \
         1 - (num_full_rack_slots / total_num_rack_slots)
-    rack_usage_data['vendor_allocation_percent'] = {
-        vendor: num_rack_slots / num_full_rack_slots
-        for vendor, num_rack_slots in vendor_allocation.items()
-    }
-    rack_usage_data['model_allocation_percent'] = {
-        model: num_rack_slots / num_full_rack_slots
-        for model, num_rack_slots in model_allocation.items()
-    }
-    rack_usage_data['owner_allocation_perecent'] = {
-        owner: num_rack_slots / num_full_rack_slots
-        for owner, num_rack_slots in owner_allocation.items()
-    }
+
+    rack_usage_data['vendor_allocation'] = [
+        {
+            "vendor": vendor,
+            "allocation_percent":
+            vendor_allocation[vendor] / num_full_rack_slots
+        } for vendor in vendor_allocation
+    ]
+
+    rack_usage_data['model_allocation'] = [
+        {
+            "vendor": model.vendor,
+            "model_number": model.model_number,
+            "allocation_percent":
+            model_allocation[model] / num_full_rack_slots
+        } for model in model_allocation
+    ]
+
+    rack_usage_data['owner_allocation'] = [
+        {
+            "owner": owner,
+            "allocation_percent":
+            owner_allocation[owner] / num_full_rack_slots
+        } for owner in owner_allocation
+    ]
 
     return JsonResponse(rack_usage_data, status=HTTPStatus.OK)
