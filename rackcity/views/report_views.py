44f1f42d1c57008d0415ef333de8b736eb0f7b5d
Julia Saveliff <jms247@duke.edu>
2020-01-31 15:51:39 -0500

    report done

--- rackcity/views/report_views.py
+++ rackcity/views/report_views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from http import HTTPStatus
-from rackcity.models import Rack
+from rackcity.models import Rack, ITInstance
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 
@@ -12,22 +12,46 @@ def report_rack_usage(request):
     Get summary of rack usage: the percentage of rackspace free versus used,
     allocated per vendor, allocated per model, and allocated per owner.
     """
-    racks = Rack.objects.all()
-    rack_usage_data = {}
-
-    # Aggregate used rackspace by model
     model_allocation = {}
-    rack_usage_data['vendor_allocation_percent'] = model_allocation
-
-    # Aggregate used rackspace by vendor
     vendor_allocation = {}
-    rack_usage_data['model_allocation_percent'] = vendor_allocation
-
-    # Aggregate used rackspace by owner
     owner_allocation = {}
-    rack_usage_data['owner_allocation_perecent'] = owner_allocation
+    num_rack_slots_full = 0
+    instances = ITInstance.objects.all()
+    for instance in instances:
+        vendor = instance.model.vendor
+        model = vendor + instance.model.model_number
+        height = instance.model.height
+        owner = instance.owner
+        num_rack_slots_full += height
+        if vendor not in vendor_allocation:
+            vendor_allocation[vendor] = 0
+        vendor_allocation[vendor] += height
+        if model not in model_allocation:
+            model_allocation[model] = 0
+        model_allocation[model] += height
+        if owner not in owner_allocation:
+            owner_allocation[owner] = 0
+        owner_allocation[owner] += height
 
-    # Aggregate total used rackspace
-    rack_usage_data['free_rackspace_percent'] = 0.0
+    num_rack_slots_total = 0
+    racks = Rack.objects.all()
+    for rack in racks:
+        num_rack_slots_total += rack.height
+
+    rack_usage_data = {}
+    rack_usage_data['free_rackspace_percent'] = \
+        num_rack_slots_full / num_rack_slots_total
+    rack_usage_data['vendor_allocation_percent'] = {
+        vendor: num_rack_slots / num_rack_slots_total
+        for vendor, num_rack_slots in vendor_allocation.items()
+    }
+    rack_usage_data['model_allocation_percent'] = {
+        model: num_rack_slots / num_rack_slots_total
+        for model, num_rack_slots in model_allocation.items()
+    }
+    rack_usage_data['owner_allocation_perecent'] = {
+        owner: num_rack_slots / num_rack_slots_total
+        for owner, num_rack_slots in owner_allocation.items()
+    }
 
     return JsonResponse(rack_usage_data, status=HTTPStatus.OK)
