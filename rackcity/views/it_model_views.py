80333435e2a09159867251e72d56a541c9b143f6
Julia Saveliff <jms247@duke.edu>
2020-01-31 13:02:18 -0500

    MIRRORS WHAT WAS ON LAST VALID MERGE

--- rackcity/views/it_model_views.py
+++ rackcity/views/it_model_views.py
@@ -9,15 +9,10 @@ from rest_framework.pagination import PageNumberPagination
 from http import HTTPStatus
 from copy import copy
 import math
-<<<<<<< HEAD
 from rackcity.views.rackcity_utils import (
     is_location_full,
-    records_are_identical,
-    get_sort_arguments,
-    get_filter_arguments,
+    records_are_identical
 )
-=======
->>>>>>> parent of cc7766fa... Merge pull request #73 from brebdab/bauriemma-model-modification-validation
 
 
 @api_view(['GET'])
@@ -77,6 +72,13 @@ def model_modify(request):
         except ObjectDoesNotExist:
             failure_message += "No existing model with id="+str(id)+". "
         else:
+            # location validation
+            if not model_height_change_valid(data, existing_model):
+                failure_message = "Height change of model causes conflicts. "
+                return JsonResponse(
+                    {"failure_message": failure_message},
+                    status=HTTPStatus.NOT_ACCEPTABLE
+                )
             for field in data.keys():
                 setattr(existing_model, field, data[field])
             try:
@@ -90,6 +92,24 @@ def model_modify(request):
     },
         status=HTTPStatus.NOT_ACCEPTABLE
     )
+
+
+def model_height_change_valid(new_model_data, existing_model):
+    if 'height' not in new_model_data:
+        return True
+    if new_model_data['height'] <= existing_model.height:
+        return True
+    else:
+        instances = ITInstance.objects.filter(model=existing_model.id)
+        for instance in instances:
+            if is_location_full(
+                instance.rack.id,
+                instance.elevation,
+                new_model_data['height'],
+                instance.id
+            ):
+                return False
+        return True
 
 
 @api_view(['POST'])
@@ -149,25 +169,22 @@ def model_page(request):
             status=HTTPStatus.BAD_REQUEST,
         )
 
-    models_query = ITModel.objects
-
-    try:
-        filter_args = get_filter_arguments(request.data)
-    except Exception as error:
-        return JsonResponse(
-            {"failure_message": "Filter error: " + str(error)},
-            status=HTTPStatus.BAD_REQUEST
-        )
-    models_query = models_query.filter(**filter_args)
-
-    try:
-        sort_args = get_sort_arguments(request.data)
-    except Exception as error:
-        return JsonResponse(
-            {"failure_message": "Sort error: " + str(error)},
-            status=HTTPStatus.BAD_REQUEST
-        )
-    models = models_query.order_by(*sort_args)
+    if 'sort_by' in request.data:
+        sort_by = request.data['sort_by']
+        sort_args = []
+        for sort in sort_by:
+            if ('field' not in sort) or ('ascending' not in sort):
+                failure_message += "Must specify 'field' and 'ascending' fields. "
+                return JsonResponse(
+                    {"failure_message": failure_message},
+                    status=HTTPStatus.BAD_REQUEST,
+                )
+            field_name = sort['field']
+            order = "-" if not sort['ascending'] else ""
+            sort_args.append(order + field_name)
+        models = ITModel.objects.order_by(*sort_args)
+    else:
+        models = ITModel.objects.all()
 
     paginator = PageNumberPagination()
     paginator.page_size = request.query_params.get('page_size')
@@ -288,28 +305,6 @@ def model_bulk_upload(request):
         },
         status=HTTPStatus.OK
     )
-
-
-def records_are_identical(existing_data, new_data):
-    existing_keys = existing_data.keys()
-    new_keys = new_data.keys()
-    for key in existing_keys:
-        if (
-            key not in new_keys
-            and existing_data[key] is not None
-            and key != 'id'
-        ):
-            return False
-        if (
-            key in new_keys
-            and new_data[key] != existing_data[key]
-        ):
-            if not (
-                isinstance(existing_data[key], int)
-                and int(new_data[key]) == existing_data[key]
-            ):
-                return False
-    return True
 
 
 @api_view(['POST'])
--- rackcity/views/rackcity_utils.py
+++ rackcity/views/rackcity_utils.py
@@ -1,5 +1,4 @@
 from rackcity.models import ITInstance, ITModel, Rack
-from rackcity.api.objects import RackRangeSerializer
 
 
 def is_location_full(
@@ -79,81 +78,3 @@ def records_are_identical(existing_data, new_data):
             ):
                 return False
     return True
-
-
-def get_sort_arguments(data):
-    sort_args = []
-    if 'sort_by' in data:
-        sort_by = data['sort_by']
-        for sort in sort_by:
-            if ('field' not in sort) or ('ascending' not in sort):
-                raise Exception("Must specify 'field' and 'ascending' fields.")
-            if not isinstance(sort['field'], str):
-                raise Exception("Field 'field' must be of type string.")
-            if not isinstance(sort['ascending'], bool):
-                raise Exception("Field 'ascending' must be of type bool.")
-            field_name = sort['field']
-            order = "-" if not sort['ascending'] else ""
-            sort_args.append(order + field_name)
-    return sort_args
-
-
-def get_filter_arguments(data):
-    filter_args = {}
-    if 'filters' in data:
-        filters = data['filters']
-        for filter in filters:
-
-            if (
-                ('field' not in filter)
-                or ('filter_type' not in filter)
-                or ('filter' not in filter)
-            ):
-                raise Exception(
-                    "Must specify 'field', 'filter_type', and 'filter' fields."
-                )
-            if not isinstance(filter['field'], str):
-                raise Exception("Field 'field' must be of type string.")
-            if not isinstance(filter['filter_type'], str):
-                raise Exception("Field 'filter_type' must be of type string.")
-            if not isinstance(filter['filter'], dict):
-                raise Exception("Field 'filter' must be of type dict.")
-
-            filter_field = filter['field']
-            filter_type = filter['filter_type']
-            filter_dict = filter['filter']
-
-            if filter_type == 'text':
-                if filter_dict['match_type'] == 'exact':
-                    filter_args['{0}'.format(filter_field)] = \
-                        filter_dict['value']
-                elif filter_dict['match_type'] == 'contains':
-                    filter_args['{0}__icontains'.format(filter_field)] = \
-                        filter_dict['value']
-
-            elif filter_type == 'numeric':
-                range_value = (
-                    int(filter_dict['min']),
-                    int(filter_dict['max'])
-                )
-                filter_args['{0}__range'.format(filter_field)] = range_value  # noqa inclusive on both min, max
-
-            elif filter_type == 'rack_range':
-                range_serializer = RackRangeSerializer(data=filter_dict)
-                if not range_serializer.is_valid():
-                    raise Exception(
-                        "Invalid rack_range filter: " +
-                        str(range_serializer.errors)
-                    )
-                filter_args['rack__rack_num__range'] = \
-                    range_serializer.get_number_range()
-                filter_args['rack__row_letter__range'] = \
-                    range_serializer.get_row_range()  # noqa inclusive on both letter, number
-
-            else:
-                raise Exception(
-                    "String field 'filter_type' must be either 'text', " +
-                    "'numeric', or 'rack_range'."
-                )
-
-    return filter_args
