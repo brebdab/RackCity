06fa75b0e8ba6578606b2697d107292b4f251ccc
Julia Saveliff <jms247@duke.edu>
2020-01-31 15:58:04 -0500

    used not free

--- rackcity/views/report_views.py
+++ rackcity/views/report_views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def report_rack_usage(request):
     """
@@ -15,14 +15,14 @@ def report_rack_usage(request):
     model_allocation = {}
     vendor_allocation = {}
     owner_allocation = {}
-    num_rack_slots_full = 0
+    num_full_rack_slots = 0
     instances = ITInstance.objects.all()
     for instance in instances:
         vendor = instance.model.vendor
         model = vendor + instance.model.model_number
         height = instance.model.height
         owner = instance.owner
-        num_rack_slots_full += height
+        num_full_rack_slots += height
         if vendor not in vendor_allocation:
             vendor_allocation[vendor] = 0
         vendor_allocation[vendor] += height
@@ -33,24 +33,24 @@ def report_rack_usage(request):
             owner_allocation[owner] = 0
         owner_allocation[owner] += height
 
-    num_rack_slots_total = 0
+    total_num_rack_slots = 0
     racks = Rack.objects.all()
     for rack in racks:
-        num_rack_slots_total += rack.height
+        total_num_rack_slots += rack.height
 
     rack_usage_data = {}
     rack_usage_data['free_rackspace_percent'] = \
-        num_rack_slots_full / num_rack_slots_total
+        1 - (num_full_rack_slots / total_num_rack_slots)
     rack_usage_data['vendor_allocation_percent'] = {
-        vendor: num_rack_slots / num_rack_slots_total
+        vendor: num_rack_slots / num_full_rack_slots
         for vendor, num_rack_slots in vendor_allocation.items()
     }
     rack_usage_data['model_allocation_percent'] = {
-        model: num_rack_slots / num_rack_slots_total
+        model: num_rack_slots / num_full_rack_slots
         for model, num_rack_slots in model_allocation.items()
     }
     rack_usage_data['owner_allocation_perecent'] = {
-        owner: num_rack_slots / num_rack_slots_total
+        owner: num_rack_slots / num_full_rack_slots
         for owner, num_rack_slots in owner_allocation.items()
     }
 
