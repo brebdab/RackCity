a94eaf86c699b6277225792edcff52ccf97e5e9a
Julia Saveliff <jms247@duke.edu>
2020-01-30 22:17:24 -0500

    i dislike re

--- rackcity/models/it_instance.py
+++ rackcity/models/it_instance.py
@@ -1,7 +1,36 @@
+import re
 from django.core.exceptions import ValidationError
 from django.db import models
 from .it_model import ITModel
 from .rack import Rack
+
+
+def validate_hostname(value):
+    # ul = '\u00a1-\uffff'  # unicode letters range (must not be a raw string)
+
+    hostname_re = r'[a-z' + \
+        r'0-9](?:[a-z' + r'0-9-]{0,61}[a-z' + r'0-9])?'
+    # Max length for domain name labels is 63 characters per RFC 1034 sec. 3.1
+    domain_re = r'(?:\.(?!-)[a-z' + r'0-9-]{1,63}(?<!-))*'
+    tld_re = (
+        r'\.'                                # dot
+        r'(?!-)'                             # can't start with a dash
+        r'(?:[a-z' + '-]{2,63}'         # domain label
+        r'|xn--[a-z0-9]{1,59})'              # or punycode label
+        r'(?<!-)'                            # can't end with a dash
+        r'\.?'                               # may have a trailing dot
+    )
+    host_re = '(' + hostname_re + domain_re + tld_re + '|localhost)'
+    host_pattern = re.compile(host_re)
+    return host_pattern
+    # matches = host_pattern.match(value)
+    # return matches
+
+    # if value % 2 != 0:
+    #     raise ValidationError(
+    #         _('%(value)s is not an even number'),
+    #         params={'value': value},
+    #     )
 
 
 class ITInstance(models.Model):
@@ -24,11 +53,3 @@ class ITInstance(models.Model):
     class Meta:
         ordering = ['hostname']
         verbose_name = 'instance'
-
-
-def validate_hostname(value):
-    if value % 2 != 0:
-        raise ValidationError(
-            _('%(value)s is not an even number'),
-            params={'value': value},
-        )
