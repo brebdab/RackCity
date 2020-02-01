7d70f951fcef8996b815a62a7fa32e0fa8402d28
Julia Saveliff <jms247@duke.edu>
2020-01-31 23:42:42 -0500

    migrate new change

--- rackcity/models/it_instance.py
+++ rackcity/models/it_instance.py
@@ -6,31 +6,10 @@ from .rack import Rack
 
 
 def validate_hostname(value):
-    # ul = '\u00a1-\uffff'  # unicode letters range (must not be a raw string)
-
-    hostname_re = r'[a-z' + \
-        r'0-9](?:[a-z' + r'0-9-]{0,61}[a-z' + r'0-9])?'
-    # Max length for domain name labels is 63 characters per RFC 1034 sec. 3.1
-    domain_re = r'(?:\.(?!-)[a-z' + r'0-9-]{1,63}(?<!-))*'
-    tld_re = (
-        r'\.'                                # dot
-        r'(?!-)'                             # can't start with a dash
-        r'(?:[a-z' + '-]{2,63}'         # domain label
-        r'|xn--[a-z0-9]{1,59})'              # or punycode label
-        r'(?<!-)'                            # can't end with a dash
-        r'\.?'                               # may have a trailing dot
-    )
-    host_re = '(' + hostname_re + domain_re + tld_re + '|localhost)'
-    host_pattern = re.compile(host_re)
-    return host_pattern
-    # matches = host_pattern.match(value)
-    # return matches
-
-    # if value % 2 != 0:
-    #     raise ValidationError(
-    #         _('%(value)s is not an even number'),
-    #         params={'value': value},
-    #     )
+    hostname_pattern = re.compile("[a-z][a-z0-9-]{0,61}[a-z0-9]")
+    if hostname_pattern.fullmatch(value) is None:
+        raise ValidationError(value + " is not a valid hostname as it is " +
+                              "not compliant with RFC 1034")
 
 
 class ITInstance(models.Model):
