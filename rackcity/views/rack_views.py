3b61a2e7cdd23c0c31d228682c9a5dcdca21119e
Julia Saveliff <jms247@duke.edu>
2020-02-04 17:18:45 -0500

    specific error messages for racks

--- rackcity/views/rack_views.py
+++ rackcity/views/rack_views.py
@@ -1,4 +1,4 @@
-# from rest_framework.parsers import JSONParser
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse, JsonResponse
 from rackcity.models import Rack, ITInstance
 from rackcity.api.serializers import RackSerializer, ITInstanceSerializer, RecursiveITInstanceSerializer
@@ -72,8 +72,14 @@ def rack_create(request):
         row_letter__range=range_serializer.get_row_range(),
     )
     if racks.count() > 0:
-        failure_message = "Racks cannot be created because racks within " + \
-            "the range already exist."
+        failure_message = "The range of racks " + \
+            range_serializer.get_row_range_as_string() + " " + \
+            range_serializer.get_number_range_as_string() + \
+            " cannot be created because the following racks" + \
+            " within this range already exist: "
+        failure_message += ", ".join(
+            [str(rack.row_letter) + str(rack.rack_num) for rack in racks]
+        )
         return JsonResponse(
             {"failure_message": failure_message},
             status=HTTPStatus.BAD_REQUEST,
@@ -94,51 +100,55 @@ def rack_delete(request):
     """
     Delete racks within specified range.
     """
+    failure_message = ""
     range_serializer = RackRangeSerializer(data=request.data)
     if not range_serializer.is_valid():
         return JsonResponse(
             {"failure_message": str(range_serializer.errors)},
             status=HTTPStatus.BAD_REQUEST,
         )
+    nonexistent_racks = []
+    unempty_racks = []
+    for row_letter in range_serializer.get_row_list():
+        for rack_num in range_serializer.get_number_list():
+            try:
+                rack = Rack.objects.get(
+                    row_letter=row_letter,
+                    rack_num=rack_num,
+                )
+            except ObjectDoesNotExist:
+                nonexistent_racks.append(row_letter + str(rack_num))
+            else:
+                if ITInstance.objects.filter(rack=rack.id).count() > 0:
+                    unempty_racks.append(row_letter + str(rack_num))
+    if len(nonexistent_racks) > 0:
+        failure_message += "The following racks within this" + \
+            " range do not exist: " + ", ".join(nonexistent_racks) + ". "
+    if len(unempty_racks) > 0:
+        failure_message += "The following racks within this" + \
+            " range contain instances: " + ", ".join(unempty_racks) + ". "
+    if failure_message != "":
+        failure_message = "The range of racks " + \
+            range_serializer.get_row_range_as_string() + " " + \
+            range_serializer.get_number_range_as_string() + \
+            " cannot be deleted. " + failure_message
+        return JsonResponse(
+            {"failure_message": failure_message},
+            status=HTTPStatus.BAD_REQUEST,
+        )
     racks = Rack.objects.filter(
         rack_num__range=range_serializer.get_number_range(),  # inclusive range
         row_letter__range=range_serializer.get_row_range(),
     )
-    if racks.count() == 0:
-        failure_message = "No racks were deleted because no racks within" + \
-            " the specified range exist."
+    try:
+        racks.delete()
+    except Exception as error:
+        failure_message = str(error)
         return JsonResponse(
             {"failure_message": failure_message},
             status=HTTPStatus.BAD_REQUEST,
         )
-    elif racks.count() < range_serializer.get_num_racks_in_range():
-        failure_message = "No racks were deleted because some racks within" + \
-            " the specified range do not exist."
-        return JsonResponse(
-            {"failure_message": failure_message},
-            status=HTTPStatus.BAD_REQUEST,
-        )
-    unempty_racks = []
-    for rack in racks:
-        if ITInstance.objects.filter(rack=rack.id).count() > 0:
-            unempty_racks.append(rack.row_letter + str(rack.rack_num))
-    if len(unempty_racks) > 0:
-        failure_message = "No racks were deleted because the following " + \
-            "racks contain instances: " + ", ".join(unempty_racks)
-        return JsonResponse(
-            {"failure_message": failure_message},
-            status=HTTPStatus.BAD_REQUEST,
-        )
-    else:
-        try:
-            racks.delete()
-        except Exception as error:
-            failure_message = str(error)
-            return JsonResponse(
-                {"failure_message": failure_message},
-                status=HTTPStatus.BAD_REQUEST,
-            )
-        return HttpResponse(status=HTTPStatus.OK)
+    return HttpResponse(status=HTTPStatus.OK)
 
 
 @api_view(['GET'])
