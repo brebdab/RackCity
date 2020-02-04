d6fd340ee9ed3b969431977c64a7b3547bb4bdc3
Benjamin Auriemma <benjamin.auriemma@duke.edu>
2020-02-04 16:56:01 -0500

    changed the model field to call super in the default case

--- rackcity/models/fields/rackcity_model_fields.py
+++ rackcity/models/fields/rackcity_model_fields.py
@@ -3,7 +3,6 @@ from django.db.models import PositiveIntegerField
 
 class RCPositiveIntegerField(PositiveIntegerField):
     def get_prep_value(self, value):
-        print("running custom get_prep_value")
         if value == '':
             return None
-        return value  # call super?
+        return super().get_prep_value(value)
