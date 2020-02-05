47f537c991c739e65b1e8d3a64587601f43efa0f
Julia Saveliff <jms247@duke.edu>
2020-02-04 19:54:40 -0500

    add validator

--- rackcity/models/rack.py
+++ rackcity/models/rack.py
@@ -1,8 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+import re
+
+
+def validate_rack_letter(value):
+    letter_pattern = re.compile("[A-Za-z]")
+    if letter_pattern.fullmatch(value) is None:
+        raise ValidationError(value + " is not a valid row letter")
 
 
 class Rack(models.Model):
-    row_letter = models.CharField(max_length=1)
+    row_letter = models.CharField(
+        max_length=1,
+        validators=[validate_rack_letter],
+    )
     rack_num = models.PositiveIntegerField()
     height = models.PositiveIntegerField(default=42)
 
