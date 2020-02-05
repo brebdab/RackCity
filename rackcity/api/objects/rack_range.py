b139bd8402861e7e4f668ba316926af0d7667186
Julia Saveliff <jms247@duke.edu>
2020-02-04 20:24:45 -0500

    invalidate negative rack number

--- rackcity/api/objects/rack_range.py
+++ rackcity/api/objects/rack_range.py
@@ -9,13 +9,17 @@ class RackRangeSerializer(serializers.Serializer):
     num_end = serializers.IntegerField(required=False)
 
     def validate(self, data):
-        if ('letter_start') in data:
+        if ('letter_start' in data):
             data['letter_start'] = data['letter_start'].upper()
             validate_row_letter(data['letter_start'])
 
-        if ('letter_end') in data:
+        if ('letter_end' in data):
             data['letter_end'] = data['letter_end'].upper()
             validate_row_letter(data['letter_end'])
+
+        if data['num_start'] <= 0:
+            message = "invalid rack number less than or equal to 0"
+            raise serializers.ValidationError(message)
 
         if ('num_end' in data) and (
             data['num_end'] < data['num_start']
