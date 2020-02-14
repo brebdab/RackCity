7cc2d746112e17607fe927d45fc454d0a5dd65b0
Julia Saveliff <jms247@duke.edu>
2020-02-13 22:00:05 -0500

    copy paste error lol

--- rackcity/api/serializers/datacenter_serializers.py
+++ rackcity/api/serializers/datacenter_serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rackcity.models import Datacenter
 
 
-class RackSerializer(serializers.ModelSerializer):
+class DatacenterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Datacenter
         fields = (
