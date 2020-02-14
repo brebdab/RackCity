d7fec0d64e8682583a8bf9b3f82d51ffdb5aba3f
Brendan Cheng <bmc41@duke.edu>
2020-02-14 16:26:03 -0500

    Created datacenter_add endpoint

--- rackcity/views/datacenter_views.py
+++ rackcity/views/datacenter_views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from rackcity.models import Datacenter
 from rackcity.api.serializers import DatacenterSerializer
 from rest_framework.decorators import permission_classes, api_view
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from http import HTTPStatus
+from rest_framework.parsers import JSONParser
 
 
 @api_view(['GET'])
@@ -17,4 +18,33 @@ def datacenter_all(request):
     return JsonResponse(
         {"datacenters": serializer.data},
         status=HTTPStatus.OK
+    )
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def datacenter_create(request):
+    """
+    Add a datacenter.
+    """
+    print(request)
+    data = JSONParser().parse(request)
+    failure_message = ""
+    if 'id' in data:
+        failure_message += "Don't include id when adding a datacenter"
+
+    serializer = DatacenterSerializer(data=data)
+    if not serializer.is_valid(raise_exception=False):
+        failure_message += str(serializer.errors)
+    if failure_message == "":
+        try:
+            serializer.save()
+            return HttpResponse(status=HTTPStatus.CREATED)
+        except Exception as error:
+            failure_message += str(error)
+
+    failure_message = "Request was invalid. " + failure_message
+    return JsonResponse(
+        {"failure_message": failure_message},
+        status=HTTPStatus.BAD_REQUEST,
     )
