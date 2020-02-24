cacb86b0f31fcf1956ed815c79973e5b9b7c292d
Brendan Cheng <bmc41@duke.edu>
2020-02-24 17:08:59 -0500

    Fixed linting issue, set up regex for keeping track of file extension

--- src/components/import/import.tsx
+++ src/components/import/import.tsx
@@ -440,7 +440,7 @@ export class BulkImport extends React.PureComponent<
 
 async function uploadBulk(encodedFile: string, token: string, type: string) {
   const url = type === "network connections" ?
-    API_ROOT + "api/" + "assets" + "/network-bulk-upload"
+    API_ROOT + "api/assets/network-bulk-upload"
     : API_ROOT + "api/" + type + "/bulk-upload";
   console.log(url);
   console.log(token);
