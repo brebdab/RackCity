59715dac55f80c4cf38033b6b26e7f9777d55ceb
Julia Saveliff <jms247@duke.edu>
2020-03-25 15:48:02 -0400

    helper method done

---
+++ rackcity/utils/change_planner_utils.py
@@ -0,0 +1,19 @@
+from django.db.models import Q
+from rackcity.models import Asset, AssetCP
+
+
+def get_assets_for_branch(change_plan=None):
+    """
+    If a change plan is specified, returns Asset query and AssetCP query,
+    where any assets modified in the change plan are in the AssetCP query but
+    removed from the Asset query. If no change plan is specified, returns
+    Asset query of all assets on master.
+    """
+    assets = Asset.objects.all()
+    if change_plan is None:
+        return (assets, None)
+    assetsCP = AssetCP.objects.filter(change_plan=change_plan)
+    for assetCP in assetsCP:
+        if (assetCP.related_asset) and (assetCP.related_asset) in assets:
+            assets = assets.filter(~Q(id=assetCP.related_asset.id))
+    return (assets, assetsCP)
