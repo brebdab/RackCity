8e665feef5fe798c976997fa1c80d16a00e48a55
Julia Saveliff <jms247@duke.edu>
2020-04-06 20:35:11 -0400

    update utils

--- rackcity/utils/execute_change_planner_utils.py
+++ rackcity/utils/execute_change_planner_utils.py
@@ -28,12 +28,8 @@ def get_updated_asset(asset_cp):
         updated_asset = Asset()
         created = True
     for field in Asset._meta.fields:
-        if (field.name != 'id' and field.name != "assetid_ptr"):
-            setattr(
-                updated_asset,
-                field.name,
-                getattr(asset_cp, field.name)
-            )
+        if field.name != "id" and field.name != "assetid_ptr":
+            setattr(updated_asset, field.name, getattr(asset_cp, field.name))
     # Assigns asset number, creates network & power ports on save
     updated_asset.save()
     if not related_asset:
@@ -45,26 +41,21 @@ def get_updated_asset(asset_cp):
 
 def update_network_ports(updated_asset, asset_cp, change_plan):
     network_ports_cp = NetworkPortCP.objects.filter(
-        change_plan=change_plan,
-        asset=asset_cp,
+        change_plan=change_plan, asset=asset_cp,
     )
     for network_port_cp in network_ports_cp:
         network_port_to_update = NetworkPort.objects.get(
-            asset=updated_asset,
-            port_name=network_port_cp.port_name,
+            asset=updated_asset, port_name=network_port_cp.port_name,
         )
         if network_port_cp.connected_port:
-            destination_host = \
-                network_port_cp.connected_port.asset.related_asset
+            destination_host = network_port_cp.connected_port.asset.related_asset
             if destination_host:
                 destination_port_to_connect = NetworkPort.objects.get(
                     asset=destination_host,
                     port_name=network_port_cp.connected_port.port_name,
                 )
-                network_port_to_update.connected_port = \
-                    destination_port_to_connect
-                destination_port_to_connect.connected_port = \
-                    network_port_to_update
+                network_port_to_update.connected_port = destination_port_to_connect
+                destination_port_to_connect.connected_port = network_port_to_update
                 destination_port_to_connect.save()
         else:
             network_port_to_update.connected_port = None
@@ -74,13 +65,11 @@ def update_network_ports(updated_asset, asset_cp, change_plan):
 
 def update_power_ports(updated_asset, asset_cp, change_plan):
     power_ports_cp = PowerPortCP.objects.filter(
-        change_plan=change_plan,
-        asset=asset_cp,
+        change_plan=change_plan, asset=asset_cp,
     )
     for power_port_cp in power_ports_cp:
         power_port_to_update = PowerPort.objects.get(
-            asset=updated_asset,
-            port_name=power_port_cp.port_name,
+            asset=updated_asset, port_name=power_port_cp.port_name,
         )
         pdu_port_cp = power_port_cp.power_connection
         if pdu_port_cp:
@@ -97,34 +86,29 @@ def update_power_ports(updated_asset, asset_cp, change_plan):
 
 def decommission_asset_cp(updated_asset, asset_cp, change_plan):
     asset_data = RecursiveAssetSerializer(updated_asset).data
-    asset_data['live_id'] = asset_data['id']
-    del asset_data['id']
-    asset_data['decommissioning_user'] = str(change_plan.owner)
+    asset_data["live_id"] = asset_data["id"]
+    del asset_data["id"]
+    asset_data["decommissioning_user"] = str(change_plan.owner)
     decommissioned_asset = AddDecommissionedAssetSerializer(data=asset_data)
     if not decommissioned_asset.is_valid(raise_exception=False):
         return JsonResponse(
             {
-                "failure_message":
-                    Status.INVALID_INPUT.value +
-                    parse_serializer_errors(decommissioned_asset.errors),
-                "errors": str(decommissioned_asset.errors)
+                "failure_message": Status.INVALID_INPUT.value
+                + parse_serializer_errors(decommissioned_asset.errors),
+                "errors": str(decommissioned_asset.errors),
             },
-            status=HTTPStatus.BAD_REQUEST
+            status=HTTPStatus.BAD_REQUEST,
         )
     try:
         decommissioned_asset.save()
     except Exception as error:
         return JsonResponse(
             {
-                "failure_message":
-                    Status.DECOMMISSION_ERROR.value +
-                    parse_save_validation_error(
-                        error,
-                        "Decommissioned Asset "
-                    ),
-                "errors": str(error)
+                "failure_message": Status.DECOMMISSION_ERROR.value
+                + parse_save_validation_error(error, "Decommissioned Asset "),
+                "errors": str(error),
             },
-            status=HTTPStatus.BAD_REQUEST
+            status=HTTPStatus.BAD_REQUEST,
         )
     else:
         updated_asset.delete()
