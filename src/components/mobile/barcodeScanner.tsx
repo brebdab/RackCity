df7e8cd64dab0ea734f5ef76641a4bb127848fbf
Brendan Cheng <bmc41@duke.edu>
2020-04-09 01:12:59 -0400

    Testing rudimentary camera scanner over https

--- src/components/mobile/barcodeScanner.tsx
+++ src/components/mobile/barcodeScanner.tsx
@@ -1,36 +1,40 @@
 import {
-  Alert,
-  AnchorButton,
-  Callout,
-  Classes,
-  Intent,
-  IToastProps,
-  Position,
-  Toaster
+  //   Alert,
+  //   AnchorButton,
+  //   Callout,
+  Classes
+  //   Intent,
+  //   IToastProps,
+  //   Position,
+  //   Toaster
 } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
-import axios from "axios";
+// import axios from "axios";
 import * as React from "react";
 import { connect } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router";
-import { API_ROOT } from "../../utils/api-config";
-import {
-  AssetObject,
-  ElementType,
-  getHeaders,
-  NetworkConnection,
-  Node,
-  DatacenterObject,
-  ROUTES
-} from "../../utils/utils";
-import PropertiesView from "../elementView/detailedView/propertiesView";
-import DecommissionedPropertiesView from "../elementView/detailedView/decommissionedPropertiesView";
+// import { API_ROOT } from "../../utils/api-config";
+// import {
+//   AssetObject,
+//   ElementType,
+//   getHeaders,
+//   NetworkConnection,
+//   Node,
+//   DatacenterObject,
+//   ROUTES
+// } from "../../utils/utils";
+// import PropertiesView from "../elementView/detailedView/propertiesView";
+// import DecommissionedPropertiesView from "../elementView/detailedView/decommissionedPropertiesView";
 // import "./assetView.scss";
-import NetworkGraph from "../elementView/detailedView/assetView/graph";
-import PowerView from "../elementView/powerView/powerView";
-import { isNullOrUndefined } from "util";
+// import NetworkGraph from "../elementView/detailedView/assetView/graph";
+// import PowerView from "../elementView/powerView/powerView";
+// import { isNullOrUndefined } from "util";
+// import BarcodeReader from "react-barcode-reader";
+import Webcam from "react-webcam";
 
-interface BarcodeScannerState {}
+interface BarcodeScannerState {
+  result: string;
+}
 interface BarcodeScannerProps {
   token: string;
   isMobile: boolean;
@@ -40,10 +44,54 @@ export class BarcodeScanner extends React.PureComponent<
   RouteComponentProps & BarcodeScannerProps,
   BarcodeScannerState
 > {
+  public state = {
+    result: ""
+  };
+
+  handleScan(data: string) {
+    this.setState({
+      result: data
+    });
+  }
+  handleError(err: any) {
+    console.error(err);
+  }
+
   render() {
+    const constraints = {
+      height: 720,
+      width: 1280,
+      facingMode: "user"
+      //   facingMode: { exact: "environment" }
+    };
+
+    const WebcamCapture = () => {
+      const webcamRef = React.useRef(null);
+      //   const webcamRef = React.createRef<HTMLVideoElement>();
+      const capture = React.useCallback(() => {
+        // alert(webcamRef);
+        alert(JSON.stringify(webcamRef));
+      }, [webcamRef]);
+      return (
+        <>
+          <Webcam
+            audio={false}
+            height={720}
+            screenshotFormat={"image/jpeg"}
+            width={1280}
+            videoConstraints={constraints}
+            mirrored={true}
+          />
+          <button onClick={capture}>Capture photo</button>
+        </>
+      );
+    };
+
     return (
       <div className={Classes.DARK}>
         <p>Scanner</p>
+        {/* <BarcodeReader onError={this.handleError} onScan={this.handleScan} /> */}
+        <WebcamCapture />
       </div>
     );
   }
