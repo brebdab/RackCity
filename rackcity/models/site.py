2d529bfc2a1fca816ad42e193eae4d06995630b5
Julia Saveliff <jms247@duke.edu>
2020-04-10 10:34:27 -0400

    add helper methods to site model

--- rackcity/models/site.py
+++ rackcity/models/site.py
@@ -5,3 +5,11 @@ class Site(models.Model):
     abbreviation = models.CharField(max_length=6, unique=True)
     name = models.CharField(max_length=150, unique=True)
     is_storage = models.BooleanField(default=False)
+
+    @staticmethod
+    def get_datacenters():
+        return Site.objects.filter(is_storage=False)
+
+    @staticmethod
+    def get_offline_storage_sites():
+        return Site.objects.filter(is_storage=True)
