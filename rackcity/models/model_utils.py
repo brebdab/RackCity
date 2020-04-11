af92dec480bacc95142b3c53c66e95b9249d6ae9
Benjamin Auriemma <benjamin.auriemma@duke.edu>
2020-04-11 03:03:28 -0400

    added validation to network port naming

--- rackcity/models/model_utils.py
+++ rackcity/models/model_utils.py
@@ -9,3 +9,9 @@ def validate_display_color(value):
         color_pattern = re.compile("#[A-Fa-f0-9]{6}")
         if color_pattern.fullmatch(value) is None:
             raise ValidationError(value + " is not a valid hex color")
+
+def validate_portname(value):
+    whitespace = re.compile("\s")
+    if whitespace.search(value) is not None:
+        raise ValidationError("Port name must be whitespace free. ")
+
