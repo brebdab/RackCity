3cfa5582e7de1a829d8dd3330f43703ba7912fc3
Julia Saveliff <jms247@duke.edu>
2020-04-14 00:06:13 -0400

    buttons and css

--- src/components/elementView/powerView/bladePowerView.tsx
+++ src/components/elementView/powerView/bladePowerView.tsx
@@ -1,12 +1,24 @@
 import * as React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
-import { Callout, Intent, IToastProps, Toaster } from "@blueprintjs/core";
+import {
+  AnchorButton,
+  Callout,
+  Intent,
+  IToastProps,
+  Toaster,
+} from "@blueprintjs/core";
 import axios from "axios";
 import { API_ROOT } from "../../../utils/api-config";
 import { connect } from "react-redux";
-import { AssetObject, ChangePlan } from "../../../utils/utils";
+import {
+  AssetObject,
+  ChangePlan,
+  getChangePlanRowStyle,
+  getHeaders,
+} from "../../../utils/utils";
 import { PermissionState } from "../../../utils/permissionUtils";
 import { IconNames } from "@blueprintjs/icons";
+import "./bladePowerView.scss";
 
 interface BladePowerViewProps {
   token: string;
@@ -78,7 +90,124 @@ export class BladePowerView extends React.PureComponent<
     }
   }
 
-  private renderNoNetworkPortCallout() {
+  private requestPowerStatus() {
+    axios
+      .post(
+        API_ROOT + "api/chassis-power/status",
+        {
+          chassis_id: this.props.asset!.chassis!.id,
+          blade_slot: this.props.asset!.chassis_slot,
+        },
+        getHeaders(this.props.token)
+      )
+      .then((res) => {
+        console.log(res.data.success_message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  private requestPowerOn() {
+    axios
+      .post(
+        API_ROOT + "api/chassis-power/on",
+        {
+          chassis_id: this.props.asset!.chassis!.id,
+          blade_slot: this.props.asset!.chassis_slot,
+        },
+        getHeaders(this.props.token)
+      )
+      .then((res) => {
+        console.log(res.data.success_message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  private requestPowerOff() {
+    axios
+      .post(
+        API_ROOT + "api/chassis-power/off",
+        {
+          chassis_id: this.props.asset!.chassis!.id,
+          blade_slot: this.props.asset!.chassis_slot,
+        },
+        getHeaders(this.props.token)
+      )
+      .then((res) => {
+        console.log(res.data.success_message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  private requestPowerCycle() {
+    axios
+      .post(
+        API_ROOT + "api/chassis-power/cycle",
+        {
+          chassis_id: this.props.asset!.chassis!.id,
+          blade_slot: this.props.asset!.chassis_slot,
+        },
+        getHeaders(this.props.token)
+      )
+      .then((res) => {
+        console.log(res.data.success_message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  private renderPowerTable() {
+    return (
+      <div className="blade-power-table">
+        <div className="network-connections">
+          <table className="bp3-html-table bp3-html-table-bordered bp3-html-table-striped">
+            <tr>
+              <th>Chassis Hostname</th>
+              <th>Chassis Slot</th>
+              <th>Power Status</th>
+            </tr>
+            <tbody>
+              <tr>
+                <td style={getChangePlanRowStyle(this.props.asset)}>
+                  {this.props.asset?.chassis?.hostname}
+                </td>
+                <td style={getChangePlanRowStyle(this.props.asset)}>
+                  {this.props.asset?.chassis_slot}
+                </td>
+                <td style={getChangePlanRowStyle(this.props.asset)}>
+                  Status TBD
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <AnchorButton
+          className={"blade-power-close"}
+          intent={"primary"}
+          minimal
+          text={"Turn on"}
+          icon="power"
+          disabled={false}
+        />
+        <AnchorButton
+          className={"blade-power-close"}
+          intent={"warning"}
+          minimal
+          text={"Cycle Power"}
+          icon="power"
+          disabled={false}
+        />
+      </div>
+    );
+  }
+
+  private renderNoPowerCallout() {
     return (
       <Callout
         title="Power is not network controllable in this blade chassis"
@@ -94,8 +223,10 @@ export class BladePowerView extends React.PureComponent<
         {this.props.asset &&
         this.props.asset.chassis &&
         this.isBladePowerNetworkControlled()
-          ? null
-          : this.renderNoNetworkPortCallout()}
+          ? this.renderNoPowerCallout()
+          : this.renderPowerTable()}
+        {/*? this.renderPowerTable()*/}
+        {/* this.renderNoPowerCallout()}*/}
       </div>
     );
   }
