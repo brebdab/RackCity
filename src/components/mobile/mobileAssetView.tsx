f4393b793f7eacdf2b163c55162dd9d3cfdad8c4
Brendan Cheng <bmc41@duke.edu>
2020-04-16 23:37:08 -0400

    Seeing if we can access token

--- src/components/mobile/mobileAssetView.tsx
+++ src/components/mobile/mobileAssetView.tsx
@@ -85,27 +85,25 @@ export class MobileAssetView extends React.PureComponent<
     loading: false,
   };
   successfullyLoadedData = false;
-  private updateAsset = (asset: AssetObject, headers: any): Promise<any> => {
-    let params: any;
-    params = this.props.match.params;
-    return modifyAsset(asset, headers, this.props.changePlan).then(
-      (res: any) => {
-        if (res.data.warning_message) {
-          this.addWarnToast("Modifed asset. " + res.data.warning_message);
-        } else {
-          this.addSuccessToast(res.data.success_message);
-        }
-
-        this.getData(params.rid, this.props.changePlan);
-
-        this.handleFormClose();
-        this.props.markTablesStale([
-          TableType.RACKED_ASSETS,
-          TableType.STORED_ASSETS,
-        ]);
-      }
-    );
-  };
+  // private updateAsset = (asset: AssetObject, headers: any): Promise<any> => {
+  //   let params: any;
+  //   params = this.props.match.params;
+  //   return modifyAsset(asset, headers, this.props.changePlan).then(
+  //     (res: any) => {
+  //       if (res.data.warning_message) {
+  //         this.addWarnToast("Modifed asset. " + res.data.warning_message);
+  //       } else {
+  //         this.addSuccessToast(res.data.success_message);
+  //       }
+  //
+  //       this.getData(params.rid, this.props.changePlan);
+  //       this.props.markTablesStale([
+  //         TableType.RACKED_ASSETS,
+  //         TableType.STORED_ASSETS,
+  //       ]);
+  //     }
+  //   );
+  // };
   private toaster: Toaster = {} as Toaster;
   private addToast(toast: IToastProps) {
     toast.timeout = 5000;
@@ -227,6 +225,8 @@ export class MobileAssetView extends React.PureComponent<
     }
     console.log(this.state);
     console.log(this.props);
+    const token = this.props.location.state
+    console.log(token)
     return (
       <div className={Classes.DARK + " asset-view"}>
         <Dialog className="spinner-dialog" isOpen={this.state.loading}>
@@ -238,110 +238,6 @@ export class MobileAssetView extends React.PureComponent<
           position={Position.TOP}
           ref={this.refHandlers.toaster}
         />
-        {!this.state.asset.decommissioning_user ? (
-          <div className="detail-buttons-wrapper">
-            <div className={"detail-buttons"}>
-              <AnchorButton
-                intent="primary"
-                icon="edit"
-                text="Edit"
-                minimal
-                onClick={() => this.handleFormOpen()}
-                disabled={
-                  (isAssetCPObject(this.state.asset) &&
-                    this.state.asset.is_decommissioned) ||
-                  !(
-                    this.props.permissionState.admin ||
-                    this.props.permissionState.asset_management ||
-                    (this.state.asset &&
-                      this.state.asset.rack &&
-                      this.props.permissionState.site_permissions.includes(
-                        +this.state.asset.rack.datacenter.id
-                      ))
-                  )
-                }
-              />
-              <FormPopup
-                datacenters={this.state.datacenters}
-                isOpen={this.state.isFormOpen}
-                initialValues={this.state.asset}
-                type={FormTypes.MODIFY}
-                elementName={ElementType.ASSET}
-                handleClose={this.handleFormClose}
-                submitForm={this.updateAsset}
-              />
-              <AnchorButton
-                minimal
-                intent="danger"
-                icon="remove"
-                text="Decommission"
-                onClick={this.handleDecommissionOpen}
-                disabled={
-                  (isAssetCPObject(this.state.asset) &&
-                    this.state.asset.is_decommissioned) ||
-                  !(
-                    this.props.permissionState.admin ||
-                    this.props.permissionState.asset_management ||
-                    (this.state.asset &&
-                      this.state.asset.rack &&
-                      this.props.permissionState.site_permissions.includes(
-                        +this.state.asset.rack.datacenter.id
-                      ))
-                  )
-                }
-              />
-              <Alert
-                cancelButtonText="Cancel"
-                confirmButtonText="Decommission"
-                intent="danger"
-                isOpen={this.state.isDecommissionOpen}
-                onCancel={this.handleDecommissionCancel}
-                onConfirm={this.handleDecommission}
-              >
-                <p>
-                  Are you sure you want to decommission this asset? If it's a
-                  chassis, its blades will also be decommissioned.
-                </p>
-              </Alert>
-              <AnchorButton
-                minimal
-                intent="danger"
-                icon="trash"
-                text="Delete"
-                onClick={this.handleDeleteOpen}
-                disabled={
-                  !isNullOrUndefined(this.props.changePlan) ||
-                  !(
-                    this.props.permissionState.admin ||
-                    this.props.permissionState.asset_management ||
-                    (this.state.asset &&
-                      this.state.asset.rack &&
-                      this.props.permissionState.site_permissions.includes(
-                        +this.state.asset.rack.datacenter.id
-                      ))
-                  )
-                }
-              />
-              <Alert
-                cancelButtonText="Cancel"
-                confirmButtonText="Delete"
-                intent="danger"
-                icon="warning-sign"
-                isOpen={this.state.isDeleteOpen}
-                onCancel={this.handleDeleteCancel}
-                onConfirm={this.handleDelete}
-              >
-                <p>
-                  Are you sure you want to <b>delete</b> this asset? Unless it
-                  was created in error, consider <b>decommissioning</b> instead.
-                </p>
-              </Alert>
-            </div>
-          </div>
-        ) : null}
-        {this.state.asset.decommissioning_user ? (
-          <DecommissionedPropertiesView data={this.state.asset} />
-        ) : null}
         {isAssetCPObject(this.state.asset) &&
         this.state.asset.is_decommissioned ? (
           <Callout
@@ -565,69 +461,6 @@ export class MobileAssetView extends React.PureComponent<
     }
   }
 
-  private handleFormOpen = () => {
-    this.setState({
-      isFormOpen: true,
-    });
-  };
-  handleFormSubmit = () => {
-    this.setState({
-      isFormOpen: false,
-    });
-  };
-
-  private handleFormClose = () => this.setState({ isFormOpen: false });
-  private handleDeleteCancel = () => this.setState({ isDeleteOpen: false });
-  private handleDeleteOpen = () => this.setState({ isDeleteOpen: true });
-  private handleDelete = () => {
-    deleteAsset(this.state.asset!, getHeaders(this.props.token))
-      .then((res: any) => {
-        this.setState({ isDeleteOpen: false });
-        this.addSuccessToast(res.data.success_message);
-        this.props.history.push(ROUTES.DASHBOARD);
-        this.props.markTablesStale([
-          TableType.RACKED_ASSETS,
-          TableType.STORED_ASSETS,
-        ]);
-      })
-      .catch((err: any) => {
-        this.addToast({
-          message: err.response.data.failure_message,
-          intent: Intent.DANGER,
-        });
-      });
-  };
-  private handleDecommissionCancel = () =>
-    this.setState({ isDecommissionOpen: false });
-  private handleDecommissionOpen = () =>
-    this.setState({ isDecommissionOpen: true });
-  private handleDecommission = () => {
-    decommissionAsset(
-      this.state.asset!,
-      getHeaders(this.props.token),
-      this.props.changePlan
-    )
-      .then((res: any) => {
-        this.setState({ isDecommissionOpen: false });
-        this.addSuccessToast("Successfully Decommissioned Asset");
-        let params: any;
-        params = this.props.match.params;
-        this.updateAssetData(params.rid);
-        this.props.markTablesStale([
-          TableType.RACKED_ASSETS,
-          TableType.STORED_ASSETS,
-          TableType.DECOMMISSIONED_ASSETS,
-        ]);
-      })
-      .catch((err: any) => {
-        console.log("there was an error: ");
-        console.log(err);
-        this.addToast({
-          message: err.response.data.failure_message,
-          intent: Intent.DANGER,
-        });
-      });
-  };
 }
 
 const mapStatetoProps = (state: any) => {
