54d8013b7ddd8306f4d653df2be67eb817f3c3c2
Brendan Cheng <bmc41@duke.edu>
2020-04-17 03:05:33 -0400

    Fixed model formatting

--- src/components/mobile/mobileAssetView.tsx
+++ src/components/mobile/mobileAssetView.tsx
@@ -21,8 +21,7 @@ interface MobileAssetViewProps {
 export class MobileAssetView extends React.PureComponent<
   MobileAssetViewProps & RouteComponentProps
 > {
-
-    renderData(fields: Array<any>, data: any) {
+  renderData(fields: Array<any>, data: any) {
     return fields.map((item: string) => {
       var dat;
       if (item === "display_color") {
@@ -124,7 +123,7 @@ export class MobileAssetView extends React.PureComponent<
             {isCustomField && item !== "display_color" ? (
               <td>{this.props.data_override[item]}</td>
             ) : (
-              <td>{dat}</td>
+              <td style={getChangePlanRowStyle(data)}>{dat}</td>
             )}
           </tr>
         );
@@ -142,116 +141,22 @@ export class MobileAssetView extends React.PureComponent<
     });
   }
 
-  // renderData(fields: Array<any>, data: any) {
-  //   return fields.map((item: string) => {
-  //     var dat;
-  //     if (item === "display_color") {
-  //       const isCustomField =
-  //         this.props.data_override && this.props.data_override[item];
-  //       const color = isCustomField
-  //         ? this.props.data_override[item]
-  //         : data[item];
-  //
-  //       dat = (
-  //         <p
-  //           className="color"
-  //           style={{
-  //             backgroundColor: color,
-  //           }}
-  //         >
-  //           {color}
-  //         </p>
-  //       );
-  //     } else if (item === "network_ports") {
-  //       if (data[item]) {
-  //         const network_ports: Array<string> = data[item];
-  //         dat = <p>{network_ports.toString()}</p>;
-  //       }
-  //     } else if (item === "model") {
-  //       dat = <p>{data[item].vendor + " " + data[item].model_number}</p>;
-  //     } else if (isAssetObject(data) && item === "rack") {
-  //       let rack = null;
-  //       if (data.rack) {
-  //         rack = data.rack;
-  //       }
-  //       if (data.chassis) {
-  //         rack = data.chassis.rack;
-  //       }
-  //       return [
-  //         <tr>
-  //           <td key={item}>
-  //             <p className="label">{AssetFieldsTable[item]}:</p>
-  //           </td>
-  //
-  //           <td style={getChangePlanRowStyle(data)}>
-  //             {" "}
-  //             <p>{rack ? rack.row_letter + "" + rack.rack_num : null}</p>
-  //           </td>
-  //         </tr>,
-  //       ];
-  //     } else if (isAssetObject(data) && item === "chassis") {
-  //       return [
-  //         <tr>
-  //           <td key={item}>
-  //             <p className="label">{AssetFieldsTable[item]}:</p>
-  //           </td>
-  //
-  //           <td style={getChangePlanRowStyle(data)}>
-  //             {" "}
-  //             <p>{data.chassis ? data.chassis.hostname : null}</p>
-  //           </td>
-  //         </tr>,
-  //       ];
-  //     } else if (item === "comment") {
-  //       dat = <p className="comment">{data[item]}</p>;
-  //     } else if (isDatacenterObject(data[item])) {
-  //       dat = <p>{data[item].name}</p>;
-  //     } else if (!isObject(data[item])) {
-  //       //TO DO: decide how to render dicts
-  //       dat = <p>{data[item]}</p>;
-  //     }
-  //
-  //     if (isAssetObject(this.props.asset)) {
-  //       return AssetFieldsTable[item] ? (
-  //         <tr>
-  //           <td key={item}>
-  //             <p className="label">{AssetFieldsTable[item]}:</p>
-  //           </td>
-  //
-  //           <td style={getChangePlanRowStyle(data)}>{dat}</td>
-  //         </tr>
-  //       ) : null;
-  //     } else {
-  //       return (
-  //         <tr>
-  //           <td key={item}>
-  //             <p className="label">{item}:</p>
-  //           </td>
-  //
-  //           <td>{dat}</td>
-  //         </tr>
-  //       );
-  //     }
-  //   });
-  // }
-
   render() {
     console.log(this.props.asset);
     return (
       <div className={Classes.DARK}>
         <Callout title={"Asset Properties"}>
           <table className="bp3-html-table">
-            {this.renderData(
-                  Object.keys(AssetFieldsTable),
-                  this.props.asset
-                )}
+            {this.renderData(Object.keys(AssetFieldsTable), this.props.asset)}
           </table>
         </Callout>
         <Callout title={"Model Properties"}>
-          {this.renderData(
+          <table className="bp3-html-table">
+            {this.renderData(
               Object.keys(ModelFieldsTable),
               this.props.asset.model
-          )}
+            )}
+          </table>
         </Callout>
         <Callout title={"Network Connections"}>
           <p>Network Connections</p>
